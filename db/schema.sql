e6ae6038b08007a6399f8891731996af7fab32b8
Arnab Majumdar <arnab@Arnabs-MacBook-Pro.local>
2018-08-29 10:14:18 -0500

    updated server

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+
+### Schema
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
