1f5d0216f5595a01ca1b6a0251a50332d17851aa
Arnab Majumdar <arnab@Arnabs-MacBook-Pro.local>
2018-08-30 16:18:15 -0500

    updated again

--- models/burger.js
+++ models/burger.js
@@ -3,9 +3,28 @@ var orm = require("../config/orm.js");
 
 // create the code that will call the ORM functions using burger specific input for the ORM.
 
-// var burger
-
-
-
-
+var burger = {
+    all: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    // delete: function(condition, cb) {
+    //   orm.delete("burgers", condition, function(res) {
+    //     cb(res);
+    //   });
+    // }
+  };
+  
 module.exports = burger;
--- server.js
+++ server.js
@@ -4,3 +4,19 @@ var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
