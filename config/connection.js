db939233bc660410959549494b1318154c3dd3d7
arnab majumdar <arnab156@gmail.com>
2018-09-06 18:00:24 -0500

    god

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,13 @@
 var mysql = require("mysql");
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "Letmein@156",
-//     database: "burgers_db",
-//    "socketPath": "/var/run/mysqld/mysqld.sock"
-//   });
+var connection = mysql.createConnection({
+    host: "gtizpe105piw2gfq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "fyzxtnr55v8cgvgw",
+    password: "bhmmlljoze5ic5ky",
+    database: "uq85bkj4z3srd5ui",
+  });
 
-var connection = mysql.createConnection(process.env.jawsdb-cubic-94621);
+// var connection = mysql.createConnection(process.env.jawsdb-cubic-94621);
   
   // Make connection.
   connection.connect(function(err) {
